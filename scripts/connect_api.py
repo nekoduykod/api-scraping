d792039218bee135b3b43c4599d96266b29fa9bf
Oleksandr Zaliubovskyi <135022571+nekoduykod@users.noreply.github.com>
2023-12-26 13:38:59 +0200

    rmvd @staticmethod and set func as simple, separate from the class

--- scripts/connect_api.py
+++ scripts/connect_api.py
@@ -14,25 +14,22 @@ class APIConnector:
         response = requests.get(full_url, params=params, headers=headers)
         return response
         
-    @staticmethod
-    def parse_installs(response):
-        data = response.replace('\\', '').replace('}]"}', '}]').replace('/', '')
-        prefix = '{"count":'       
-        count_start = data.find(prefix)
-        count_end = data.find(',"records":"') + len(',"records":"')  
-        characters_to_strip = count_end - count_start
-        data = data[characters_to_strip:]
-        return data
+def parse_installs(response):
+    data = response.replace('\\', '').replace('}]"}', '}]').replace('/', '')
+    prefix = '{"count":'       
+    count_start = data.find(prefix)
+    count_end = data.find(',"records":"') + len(',"records":"')  
+    characters_to_strip = count_end - count_start
+    data = data[characters_to_strip:]
+    return data
         
-    @staticmethod
-    def parse_costs(response):
-        data = response.replace('\t', ',').replace('/home', 'home').replace('/sale', 'sale')\
-                        .replace('/signup', 'signup').replace('/new-arrivals', 'new-arrivals').replace('/products', 'products')
-        return data
+def parse_costs(response):
+    data = response.replace('\t', ',').replace('/home', 'home').replace('/sale', 'sale')\
+                    .replace('/signup', 'signup').replace('/new-arrivals', 'new-arrivals').replace('/products', 'products')
+    return data
         
-    @staticmethod
-    def parse_events(response):
-        data = response.replace('\\', '').replace('{"data":"', '').replace('}}]",', '}}]')
-        next_page_index = data.find('"next_page"')
-        data = data[:next_page_index]
-        return data
+def parse_events(response):
+    data = response.replace('\\', '').replace('{"data":"', '').replace('}}]",', '}}]')
+    next_page_index = data.find('"next_page"')
+    data = data[:next_page_index]
+    return data
